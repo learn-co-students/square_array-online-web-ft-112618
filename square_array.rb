6d313dfb76de16423b7389e08f83b1a9f2bd5921
Andrew Shupe <adshupe@ucdavis.edu>
2018-12-01 23:25:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+array = [1, 2, 3, 4]
 def square_array(array)
-  # your code here
+   arr = []
+   array.each { |i| arr << i ** 2 }
+   arr
+end
+
+array2 = [3, 6, 9, 12]
+def square_array2(array2)
+  arr2 = []
+  array2.collect {|x| arr2 << x ** 2}
+  arr2 
 end
